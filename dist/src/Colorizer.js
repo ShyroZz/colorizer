c6d82f53b79a04971245e351b852c3fbc02bcca0
Antonin <antonin.h@globeservices.fr>
2019-02-19 18:02:32 +0100

    Add Colorizer class

---
+++ dist/src/Colorizer.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Colorizer = /** @class */ (function () {
+    function Colorizer() {
+    }
+    return Colorizer;
+}());
+exports.default = Colorizer;
---
+++ index.ts
@@ -0,0 +1,1 @@
+import Colorizer from './src/Colorizer'
